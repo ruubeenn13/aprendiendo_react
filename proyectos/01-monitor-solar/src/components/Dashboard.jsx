336f20818e04437da561ba205e71fda56cdae700
Rubén <rubenjuancandela06@gmail.com>
2026-03-24 08:20:28 +0100

    Arreglar error en Dashboard

--- proyectos/01-monitor-solar/src/components/Dashboard.jsx
+++ proyectos/01-monitor-solar/src/components/Dashboard.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import instalacion from "../data/instalacion";
 import Meteorologia from "./Meteorologia";
-import './Dashboard.css'
+import "./Dashboard.css";
 
 const Dashboard = () => {
-    return (
-        <div className="dashboard-container">
-            <div className="dashboard-header">
-                <span className="header-bar"></span>
-                <h1 className="installation-name">{instalacion.nombre}</h1>
-            </div>
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-header">
+        <span className="header-bar"></span>
+        <h1 className="installation-name">{instalacion.nombre}</h1>
+      </div>
 
-            <div className="dashboard-content">
-                <Meteorologia 
-                    temperatura={instalacion.temperatura}
-                    radiacion={instalacion.radiacion}
-                />
-            </div>
-        </div>
-    )
-}
+      <div className="dashboard-content">
+        <Meteorologia
+          temperatura={instalacion.meteorologia.temperatura}
+          radiacion={instalacion.meteorologia.radiacion}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
